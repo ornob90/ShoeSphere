f0c296e54424785b8342f42df66acfac50addb9d
ornob90 <oornob49@gmail.com>
2023-11-12 15:02:58 +0600

    seting up google font poppings

--- src/pages/Home/Navbar/Navbar.jsx
+++ src/pages/Home/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className=" font-clashLight text-sm">Home About</div>;
+  return <div className="">Navbar</div>;
 };
 
 export default Navbar;
