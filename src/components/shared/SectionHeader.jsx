407d38ec17b7584cb908e29861d4816d336a965e
ornob90 <oornob49@gmail.com>
2023-11-13 14:43:43 +0600

    section header complete

--- src/components/shared/SectionHeader.jsx
+++ src/components/shared/SectionHeader.jsx
@@ -4,9 +4,11 @@ import Button from "../html/Button";
 
 const SectionHeader = ({ title }) => {
   return (
-    <Container className="flex justify-between items-center">
-      <h1>{title}</h1>
-      <Button>View All</Button>
+    <Container className="mb-10 mt-20 flex justify-between items-center">
+      <h1 className=" font-clashBold text-xl md:text-3xl">{title}</h1>
+      <Button className="bg-white text-black border-b-2 border-black text-[12px] md:text-[14px]  px-0 py-0  font-clashMedium">
+        View All
+      </Button>
     </Container>
   );
 };
