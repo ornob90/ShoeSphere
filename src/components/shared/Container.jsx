f37feefd445806f3af07ea44c482fce6a8f869cc
ornob90 <oornob49@gmail.com>
2023-11-13 12:06:38 +0600

    banner title with responsive complete

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Container = ({ children, className }) => {
   return (
-    <div className={` w-[90%] mx-auto pt-[6%] ${className}`}>{children}</div>
+    <div className={` w-[90%] mx-auto pt-[10%] md:pt-[6%] ${className}`}>
+      {children}
+    </div>
   );
 };
 
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,15 +6,15 @@ const Banner = () => {
   return (
     <Container>
       <div className="w-full grid grid-cols-1 md:grid-cols-5 pt-10">
-        <h1 className=" font-clashBold md:col-span-3 text-2xl md:text-3xl lg:text-4xl">
+        <h1 className=" font-clashBold md:col-span-3 text-xl sm:text-2xl md:text-3xl lg:text-4xl">
           Discover Your Perfect Pair
         </h1>
         <div className="md:col-span-2 ">
-          <p className="font-poppins text-sm text-gray-400 mb-4 w-[70%]">
+          <p className="font-poppins text-[12px] md:text-sm text-gray-400 mb-4 w-[70%]">
             Explore our curated collection of the latest trends in footwear.
             Find the perfect pair that suits your style and comfort
           </p>
-          <Button className=" py-1 px-4 bg">Find Your Style</Button>
+          <Button className=" py-1 px-4 ">Find Your Style</Button>
         </div>
       </div>
     </Container>
