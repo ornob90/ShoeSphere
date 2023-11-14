2aaed60bb74e27e8c19b097b02f725c39bee92d6
ornob90 <oornob49@gmail.com>
2023-11-14 15:06:20 +0600

    responsive detail banner

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -4,7 +4,12 @@ import Container from "../../components/shared/Container";
 const ProductDetail = () => {
   return (
     <div className="min-h-screen ">
-      <div className="h-[250px] bg-yellow-300 bg-detail"></div>
+      <div className="h-[250px] bg-yellow-300 bg-detail flex justify-center items-center bg-fixed">
+        <h3 className="text-gray-400 font-clashBold text-xl md:text-2xl lg:text-3xl text-center">
+          Unveiling [Product Name]:
+          <br /> A Step Towards Timeless Elegance
+        </h3>
+      </div>
     </div>
   );
 };
