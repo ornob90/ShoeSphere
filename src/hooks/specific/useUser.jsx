2d9c38612104d699d7e2c9fa07aed0a796d2b8a6
ornob90 <oornob49@gmail.com>
2023-12-06 11:16:21 +0600

    useUser hook complete

---
+++ src/hooks/specific/useUser.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useAuth from "../auth/useAuth";
+import useGetSecure from "../apiSecure/useGetSecure";
+
+const useUser = () => {
+  const { user } = useAuth();
+
+  const { data: userData } = useGetSecure(
+    ["User", user?.email],
+    `/user/${user?.email}`
+  );
+
+  return userData;
+};
+
+export default useUser;
