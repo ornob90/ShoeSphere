345159df452fbfd31d64db442cf9460ab437e8de
ornob90 <oornob49@gmail.com>
2023-12-06 11:55:59 +0600

    brand with use state complte

--- src/pages/ProductCollection/ProductCollection.jsx
+++ src/pages/ProductCollection/ProductCollection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../../components/shared/Containers/Container";
 import ProductCollectionCarousal from "./ProductCollectionCarousal/ProductCollectionCarousal";
 import FilterDrawer from "./FilterDrawer/FilterDrawer";
@@ -23,15 +23,26 @@ const ProductCollection = () => {
     "Salomon",
   ];
 
+  const [brand, setBrand] = useState("");
+  const [genders, setGenders] = useState([]);
+  const [available, setAvailable] = useState([]);
+
+  const handleBrand = (brand) => {
+    setBrand((prev) => (prev === brand ? "" : brand));
+  };
+
   return (
     <Container className="pt-[25%] sm:pt-[20%] md:pt-[15%] lg:pt-[7%] min-h-[600px]">
       <ul className="flex w-full max-w-full gap-4 mt-5 overflow-x-scroll no-scrollbar border-y">
-        {shoeBrands.map((brand) => (
+        {shoeBrands.map((brandName) => (
           <li
-            key={brand}
-            className="py-3 w-[25%] md:w-[20%] lg:w-[13%]  flex-shrink-0 text-center hover:bg-black hover:text-white cursor-pointer duration-[.4s] text-sm flex  justify-center"
+            onClick={() => handleBrand(brandName)}
+            key={brandName}
+            className={`py-3 w-[25%] md:w-[20%] lg:w-[13%]  flex-shrink-0 text-center hover:border-black border border-white  cursor-pointer duration-[.4s] text-sm flex  justify-center ${
+              brandName === brand ? " bg-black text-white" : "text-black"
+            }`}
           >
-            {brand}
+            {brandName}
             <div>
               <img
                 src="https://i.ibb.co/PmnkYfM/nike-removebg-preview.png"
