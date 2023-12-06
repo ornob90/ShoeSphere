9a7317f40fb5e81ca25c419791fce0c8fcd17433
ornob90 <oornob49@gmail.com>
2023-12-06 19:23:01 +0600

    product added to cart

--- src/pages/ProductCollection/ProductCard/ProductCard.jsx
+++ src/pages/ProductCollection/ProductCard/ProductCard.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import { GoPlus } from "react-icons/go";
 import { FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import usePostSecure from "../../../hooks/apiSecure/usePostSecure";
+import toast from "react-hot-toast";
+import useUser from "../../../hooks/specific/useUser";
 
 const ProductCard = ({ product }) => {
   const { _id, name, price, images } = product || {};
+  const { _id: user } = useUser() || {};
+
+  const { mutateAsync: addToCart } = usePostSecure(null, "/cart");
 
   const navigate = useNavigate();
+
+  const handleAddToCart = async () => {
+    try {
+      const product = {
+        user,
+        product: _id,
+        price,
+      };
+
+      await toast.promise(addToCart(product), {
+        pending: "Adding to cart...",
+        success: "Added to cart!",
+        error: "Failed to add to cart",
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="h-[350px] ">
@@ -21,15 +46,20 @@ const ProductCard = ({ product }) => {
         />
         <FaRegHeart className="absolute top-[5%] right-[5%] text-lg" />
       </div>
-      <div className="flex flex-col gap-1 mt-3 text-sm font-medium">
+      <div className="px-2 flex flex-col gap-1 mt-3 text-sm font-medium">
         <h1 className="font-semibold">{name}</h1>
         <div className="flex items-center justify-between w-[95%]">
           <p>${price}</p>
           <p className="w-[15px] h-[15px] bg-black rounded-full"></p>
         </div>
-        <div className="flex justify-between w-[96%]">
-          <p className="text-[12px]">Add to cart</p>
-          <p className="text-2xl text-gray-400 cursor-pointer">+</p>
+        <div className="flex justify-between w-[96%] cursor-pointer">
+          <p className="text-[12px] ">Add to cart</p>
+          <p
+            onClick={handleAddToCart}
+            className="text-2xl text-gray-400 cursor-pointer    duration-300 hover:rotate-90"
+          >
+            +
+          </p>
         </div>
       </div>
     </div>
