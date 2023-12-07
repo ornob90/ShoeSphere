0aaf8bdf1d329ed10530698c6f2b8d0e69c8e1fe
ornob90 <oornob49@gmail.com>
2023-12-07 18:49:59 +0600

    product quanty update in cart drawer card

--- src/pages/CartDrawer/CartDrawerCard.jsx
+++ src/pages/CartDrawer/CartDrawerCard.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import PlusMinusBtn from "../../components/shared/SingleUseButtons/PlusMinusBtn";
+import QuantityBtn from "../../components/shared/SingleUseButtons/QuantityBtn";
+import usePutSecure from "../../hooks/apiSecure/usePutSecure";
 const CartDrawerCard = ({ cartProduct }) => {
-  const { product } = cartProduct || {};
+  const { product, quantity, price, _id } = cartProduct || {};
+
+  const [productQuantity, setProductQuantity] = useState(quantity);
+
+  const { mutateAsync: updateQuantityPrice } = usePutSecure(
+    null,
+    `/update-cart-quantity/${_id}`
+  );
+
+  const handleQuantity = async (action) => {
+    let newQuantity;
+    if (action === "+") {
+      newQuantity = productQuantity + 1;
+    } else {
+      newQuantity = productQuantity - 1;
+    }
+
+    setProductQuantity(newQuantity);
+
+    const dataToSend = {
+      quantity: newQuantity,
+      price: product?.price,
+    };
+
+    try {
+      const response = await updateQuantityPrice(dataToSend);
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="mt-5 border  flex justify-between font-poppins overflow-y-auto rounded-md">
       <div className="w-[28%] bg-[#F0F0F4]">
@@ -16,8 +49,13 @@ const CartDrawerCard = ({ cartProduct }) => {
           <RiDeleteBin6Line />
         </div>
         <div className="flex justify-between items-center">
-          <p className=" font-clashSemibold">{product?.price}$</p>
-          <PlusMinusBtn />
+          <p className=" font-clashSemibold">
+            {product?.price * productQuantity}$
+          </p>
+          <QuantityBtn
+            productQuantity={productQuantity}
+            handleQuantity={handleQuantity}
+          />
         </div>
       </div>
     </div>
