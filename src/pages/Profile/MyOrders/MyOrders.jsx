e98a7da2ac1765c5272d0b43adefab62ac0496bd
ornob90 <oornob49@gmail.com>
2023-12-09 23:30:48 +0600

    showing my orders

--- src/pages/Profile/MyOrders/MyOrders.jsx
+++ src/pages/Profile/MyOrders/MyOrders.jsx
@@ -6,46 +6,61 @@ import TableHeader from "../../../components/shared/table/TableHeader";
 import Table from "../../../components/shared/Table/Table";
 import TableBody from "../../../components/shared/Table/TableBody";
 import TableRow from "../../../components/shared/table/TableRow";
+import useGetSecure from "../../../hooks/apiSecure/useGetSecure";
+import useUser from "../../../hooks/specific/useUser";
 
 const MyOrders = () => {
-  const headers = ["ID", "Name", "Quantity", "Price", "Status"];
-  const orders = [
-    {
-      ID: "1",
-      Name: "Apples",
-      Quantity: 10,
-      Price: 2.99,
-      Status: "In Stock",
-    },
-    {
-      ID: "2",
-      Name: "Orangesasdfadfadfasfadsf",
-      Quantity: 5,
-      Price: 3.49,
-      Status: "In Stock",
-    },
-    {
-      ID: "3",
-      Name: "Bread",
-      Quantity: 8,
-      Price: 2.5,
-      Status: "Out of Stock",
-    },
-    {
-      ID: "4",
-      Name: "Milk",
-      Quantity: 15,
-      Price: 1.99,
-      Status: "In Stock",
-    },
-    {
-      ID: "5",
-      Name: "Cheese",
-      Quantity: 3,
-      Price: 4.99,
-      Status: "In Stock",
-    },
+  const headers = [
+    "OrderID",
+    "Transaction ID",
+    "Quantity",
+    "Price",
+    "Order Date",
+    "Status",
   ];
+  // const orders = [
+  //   {
+  //     ID: "1",
+  //     Name: "Apples",
+  //     Quantity: 10,
+  //     Price: 2.99,
+  //     Status: "In Stock",
+  //   },
+  //   {
+  //     ID: "2",
+  //     Name: "Orangesasdfadfadfasfadsf",
+  //     Quantity: 5,
+  //     Price: 3.49,
+  //     Status: "In Stock",
+  //   },
+  //   {
+  //     ID: "3",
+  //     Name: "Bread",
+  //     Quantity: 8,
+  //     Price: 2.5,
+  //     Status: "Out of Stock",
+  //   },
+  //   {
+  //     ID: "4",
+  //     Name: "Milk",
+  //     Quantity: 15,
+  //     Price: 1.99,
+  //     Status: "In Stock",
+  //   },
+  //   {
+  //     ID: "5",
+  //     Name: "Cheese",
+  //     Quantity: 3,
+  //     Price: 4.99,
+  //     Status: "In Stock",
+  //   },
+  // ];
+  const { _id: userID } = useUser() || {};
+  const { data: orders } = useGetSecure(
+    ["MyOrders", userID],
+    `/orders/${userID}`
+  );
+
   return (
     <DashboardProfileContainer>
       <ProfileHeader
@@ -56,18 +71,27 @@ const MyOrders = () => {
         <Table>
           <TableHeader headers={headers} cellWidth="35%" />
           <TableBody>
-            {orders?.map((row, idx) => (
-              <>
+            {orders?.map(
+              (
+                { _id, transactionID, quantity, price, orderDate, status },
+                idx
+              ) => (
                 <TableRow
+                  key={_id}
                   className=" font-[300] text-gray-700 text-sm"
-                  key={idx}
-                  cells={Object.values(row).slice(0, -1)}
+                  cells={[_id, transactionID, quantity, price, orderDate]}
                   cellWidth="35%"
                 >
-                  <th className="text-start w-[35%]">"last"</th>
+                  <th
+                    className={`text-start w-[35%] ${
+                      status === "pending" ? "text-red-500" : "text-green-600"
+                    }`}
+                  >
+                    {status}
+                  </th>
                 </TableRow>
-              </>
-            ))}
+              )
+            )}
           </TableBody>
         </Table>
       </div>
